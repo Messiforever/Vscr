678d029914f09461626baa06e524b8c58abec540
M <lyo724691463@163.com>
2017-01-10 09:24:39 +0800

    update

--- app.js
+++ app.js
@@ -3,8 +3,18 @@
  */
 var express = require('express');
 var ejs = require('ejs');  //安装ejs引擎
+var bodyParser = require('body-parser');
+
 var app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+
 app.use('/',express.static(__dirname)); //注册动态路由，定位到根目录
+app.engine('.html',ejs.__express);//指定一个html文件用ejs方式解析HTML文件
+app.set('view engine','html');
+
+
 var router = express.Router();
 router.get("/home",function (req,res) { //当用户在浏览器地址上输入home后，执行这个函数
    var result = [];
@@ -87,13 +97,6 @@ router.get("/home",function (req,res) { //当用户在浏览器地址上输入ho
       }
 
 });
-app.use('/',router);
-app.engine('.html',ejs.__express);//指定一个html文件用ejs方式解析HTML文件
-app.set('view engine','html');
-var port = 8888; //引入一个端口
-app.listen(port);
-console.log("Is running on" + port);
-
 
 router.post("/login",function (req,res) {
   req.on('data',function(data) {
@@ -106,26 +109,131 @@ router.post("/login",function (req,res) {
         var array2 = str.split("=");
         result[array2[0]] = array2[1];
       });
-      res.json(result);
+
+              if(result.username == "hello" && result.password == "a123" && result.identifyingCode=="a123") {
+                 var expireDate = new Date();
+                 var expireDays = 10;
+                expireDate.setTime(expireDate.getTime() + expireDays*24*3600*1000);
+                res.writeHead(200,{
+                  'Set-Cookie':'auth=qwer-tyui-asdf-ghjk;nickname=Messi;username=' + result.username + ';expire=' + expireDate.toGMTString(),
+                  'Content-type':'text/plain:charset=utf-8'
+                });
+                res.end(JSON.stringify({flag:true,meg:"登陆成功"}));
+
+                  }
+                  else {
+                res.json({flag:false,meg:"用户名或密码错误"});
+              }
     }
 
     Logins(loginData);
   });
 });
 router.post("/register",function (req, res) {
-  req.on('data',function(data) {
-    var registerData = decodeURIComponent(data);
+  var result = req.body;
 
-    function Register(param) {
-      var result = {};
-      var array = param.split("&");
-      array.forEach(function (str) {
-        var array2 = str.split("=");
-        result[array2[0]] = array2[1];
-      });
-      res.json(result);
-    }
+      if(result.username == "hello" && result.password == "a123" && result.identifyingPassword=="a123") {
+        var expireDate = new Date();
+        var expireDays = -1;
+        expireDate.setTime(expireDate.getTime() + expireDays*24*3600*1000);
 
-    Register(registerData);
+        res.writeHead(200,{
+          'Set-Cookie':'auth=qwer-tyui-asdf-ghjk;nickname=Messi;username=' + result.username + ';expire=' + expireDate.toGMTString(),
+          'Content-type':'text/plain:charset=utf-8'
+        });
+        res.end(JSON.stringify({flag:true,meg:"注册成功"}));
+
+      }
+      else {
+        res.json({flag:false,meg:"用户名已存在，注册失败"});
+      }
+
+
+
   });
+
+
+router.get("/chat", function (req, res) {
+  exports.chat = exports.chat || {};
+  function Message(key,m,uname,time,to) {
+    var self = this;
+    self.key = key;
+    self["user-name"] = uname;
+    self["message-time"] = time;
+    self.message = m;
+    self.to = to;
+    self.isToAll = false;
+    self.type = 1;//0透传消息，非0为显示消息
+
+  }
+  var logined;
+  if (req.query.username) {
+    logined = req.query.username;
+    exports.chat[logined] = exports.chat[logined] || {};
+    exports.chat[logined].res = res;
+    exports.chat[logined].req = req;
+    exports.chat[logined].req.on("close",function () {
+      for(var u in exports.chat){
+        if(exports.chat.hasOwnProperty(u)) { //hasOwnProperty不查找原型，只查找自己的属性
+          var m = new Message("user-offline","",logined,new Date(),"");
+          m.isToAll = true;
+          m.type = 0;
+
+          exports.chat[u].res.write("data:" + JSON.stringify(m) + "\n\n");
+        }
+
+      }
+      delete exports.chat[logined];
+    });
+
+    res.writeHead(200, {
+      "Content-Type": "text/event-stream",
+      "Cache-Contral": "no-cache",
+      "Connection": "keep-alive"
+    });
+    for (var u in exports.chat) {
+      if (exports.chat.hasOwnProperty(u)) {
+        var m = new Message(
+          "user-online",
+          "",
+          logined,
+          new Date(),
+          ""
+
+        );
+        m.isToAll = true;
+        m.type = 0;
+
+        exports.chat[u].res.write("data:" + JSON.stringify(m) + "\n\n");
+      }
+
+    }
+  }
+  //获取所有在线用户
+  if (req.query.GetALLUser) {
+    var users = [];
+
+    for (var u1 in exports.chat) {
+      if (exports.chat.hasOwnProperty(u1)) {
+        users.push(u1);
+
+      }
+    }
+    res.json(users);
+  }
+  // if (req.query.Chatting){
+  //
+  //   var a = req.query.to;
+  //   var b = req.query.sendMsg;
+  //   exports.chat[a].res.write("data:"+JSON.stringify({content:b})+"\n\n");
+  //
+  //   res.json({});
+  // }
 });
+
+
+app.use('/',router);
+
+var port = 8888; //引入一个端口
+app.listen(port);
+console.log("Is running on" + port);
--- script/index.js
+++ script/index.js
@@ -59,7 +59,7 @@ function ajaxFn(url,option,method,callback) {
     Login.appendChild(register);
     Login.appendChild(login);
     var LoginContainer1 = document.querySelector(".login > div > div:nth-child(2)");
-    LoginContainer1.innerHTML ="<form action='/register' method='post'>"+
+    LoginContainer1.innerHTML =
       "<div><h2>用户注册</h2></div>"
       +"<div><p></p></div>"
       +"<div>"+
@@ -68,25 +68,25 @@ function ajaxFn(url,option,method,callback) {
       +"<div><label for=''>确认密码：</label><input type='password' name='identifyingPassword'/></div>"
       +"<div><input type='submit' value='注册'/><input type='reset' value='取消'/></div>"
       +"<a>已有账号？点击登录</a>"
-      + "</div>"+"</form>";
+      + "</div>";
     var LoginContainer = document.querySelector(".login > div > div");
-    LoginContainer.innerHTML ="<form action='/login' method='post'>"+
+    LoginContainer.innerHTML =
       "<div><h2>用户登录</h2></div>"
       +"<div><p></p></div>"
       +"<div>"+
-      "<div><label for=''>用户名：</label><input type='text' name='username'/></div>"
+      "<div><label for='userInput'>用户名：</label><input type='text' name='username' id='userInput'/></div>"
       +"<div><label for=''>密码：</label><input type='password' name='password'/></div>"
       +"<div><label for=''>验证码：</label><input type='text' name='identifyingCode'/></div>"
-      +"<div><input type='submit' value='登录'/><input type='reset' value='取消'/></div>"
+      +"<div><input type='button' id='userLogin' value='登录' onclick='userSend()'/><input type='reset' value='取消'/></div>"
       +"<a>还没有账号？点击注册</a>"
-      + "</div>"+"</form>";
-      var cancel = document.querySelector(".login > div > div form> div:nth-child(2) p");
+      + "</div>";
+      var cancel = document.querySelector(".login > div > div > div:nth-child(2) p");
     cancel.addEventListener("click",function (e){
       e.stopPropagation();
       Login.style.visibility="hidden";
       Login.style.opacity=0;
     });
-    var cancel = document.querySelector(".login > div > div:last-child form> div:nth-child(2) p");
+    var cancel = document.querySelector(".login > div > div:last-child > div:nth-child(2) p");
     cancel.addEventListener("click",function (e){
       e.stopPropagation();
       Login.style.visibility="hidden";
@@ -105,7 +105,8 @@ function ajaxFn(url,option,method,callback) {
       LoginContainer.style.opacity=1;
       LoginContainer1.style.top="57%";
       LoginContainer.style.top="25%";
-
+      LoginContainer.style.zIndex = 2;
+      LoginContainer1.style.zIndex = 1;
     });
     var logining = document.querySelector(".login > div > div:first-child a");
     logining.addEventListener("click",function (e) {
@@ -114,10 +115,178 @@ function ajaxFn(url,option,method,callback) {
       LoginContainer.style.opacity=0;
       LoginContainer1.style.top="25%";
       LoginContainer.style.top="-7%";
-
+      LoginContainer.style.zIndex = 1;
+      LoginContainer1.style.zIndex = 2;
     });
-  });
 
+
+    // userLogin.addEventListener("click",function (e) {
+    //   e.stopPropagation();
+    //   Login.style.visibility="hidden";
+    //   Login.style.opacity=0;
+    // });
+
+    // function Chat(event) {
+    //   var nowone = event.currentTarget.innerHTML;
+    //   sendUser = nowone;
+    //   usersmessage = "<div>"+"用户："+nowone+"</div>";
+    //   if(userName == nowone) {
+    //   }else {
+    //     CreateChat();
+    //
+    //   }
+    //   return sendUser;
+    // }
+    //拖动聊天容器
+    var mouseinnerX;
+    var mouseinnerY;
+    var flag=false;
+    var mouseX;
+    var mouseY;
+    window.addEventListener("mousedown",function (e) {
+      mouseX = e.pageX;
+      mouseY = e.pageY;
+    });
+    window.addEventListener("mouseup",function () {
+      flag=false;
+    });
+    userList.addEventListener("mousedown",function () {
+      flag=true;
+    });
+    userList.addEventListener("mousemove",function (e) {
+      if(flag){
+        mouseinnerX=e.pageX;
+        mouseinnerY=e.pageY;
+        userList.style.left=userList.offsetLeft+(mouseinnerX-mouseX)+"px";
+        userList.style.top=userList.offsetTop+(mouseinnerY-mouseY)+"px";
+        mouseX=mouseinnerX;
+        mouseY=mouseinnerY;
+
+      }
+    });
+    Login1= Login;
+  });
+//添加聊天功能
+function ajax(url, method, data, callback) {
+  var xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = function () {
+    if(xhr.readyState == 4 && xhr.status ==200) {
+      callback(xhr.responseText);
+    }
+  };
+  xhr.open(method,url,true);
+  xhr.send(data);
+}
+var Login1;
+var userLogin = document.querySelector("#userLogin");
+var userInput = document.getElementById("userInput");
+var source,userName,sendUser;
+function connect(userName) {
+  source = new EventSource("/chat?username=" + userName);
+  source.addEventListener("message", function (event) {
+    var m =JSON.parse(event.data);
+    if(m.key === "user-offline") {
+      getAllOnlineUser();
+    }else     if(m.key === "user-online") {
+      getAllOnlineUser();
+    }else     if(m.content) {
+      document.querySelector("#usersmessage").innerHTML +="<div>"+sendUser+":"+m.content+"<---------"+getNowFormatDate()+"</div>";
+    }
+//        output.textContent = event.data;
+
+  }, false); //false意思是不要在捕获阶段执行，在冒泡阶段执行
+
+  // source.addEventListener("connecttime", function (event) {
+  //   connectTime.textContent = "Connection was last established at:" + event.data;
+  // }, false);
+
+  source.addEventListener("open", function (event) {
+    // button.value = "Disconnect";
+    // function userSends(event) {
+    //   // button.value = "Connect";
+    //   event.stopPropagation();
+    //   Login1.style.visibility="hidden";
+    //   Login1.style.opacity=0;
+    //   userLogin.onclick = function () {
+    //     userName = userInput.value;
+    //     connect(userName);
+    //   };
+    //   source.close();
+    //   status.textContent = "Connection closed!";
+    // };
+
+    status.textContent = "Connection open";
+  }, false);
+
+  // source.addEventListener("error", function (event) {
+  //   if (event.target.readyState === EventSource.CLOSED) {
+  //     source.close();
+  //     status.textContent = "Connection closed!";
+  //   }
+  //   else if (event.target.readyState === EventSource.CONNECTING) {
+  //     status.textContent = "Connection closed. Attempting tp reconnect!";
+  //   } else {
+  //     status.textContent = "Connection closed. Unknown error!";
+  //   }
+  // }, false);
+
+}
+var userList= document.querySelector("#dataContainer");
+var d= document.querySelector("#friends-list");
+function getAllOnlineUser() {
+  ajax("/chat?GetALLUser=true","get",null,function (users) {
+    users = JSON.parse(users);
+    if(Array.isArray(users)) {
+      var h = "";
+
+      for(var i = 0;i<users.length;i++){
+        h +="<div>"+users[i]+"</div>";
+
+      }
+      // var d= document.querySelector("#dataContainer");
+
+      d.innerHTML = h;
+
+    }
+
+  });
+}
+ajax("/chat?GetALLUser=true","get",null,function (users) {
+  users = JSON.parse(users);
+  if(Array.isArray(users)) {
+    var h = "";
+
+    for(var i = 0;i<users.length;i++){
+      h +="<div>"+users[i]+"</div>";
+
+    }
+    // var d= document.querySelector("#dataContainer");
+
+    d.innerHTML = h;
+
+  }
+
+
+});
+if (!!window.EventSource) {  //“！！”意思是双重否定表肯定，保持它的值为布尔值
+  function userSend() {
+    userName = userInput.value;
+    if (userInput != "") {
+      connect(userName);
+    } else {
+      alert("用户名不能为空！！");
+    }
+
+  }
+
+} else {
+  userLogin.style.display = "none";
+  status.textContent = "Sorry,your browser doesn't support server-sent events";
+}
+function getUserProfile() {
+ var cookies = document.cookie;
+}
+getUserProfile();
 
 
 
